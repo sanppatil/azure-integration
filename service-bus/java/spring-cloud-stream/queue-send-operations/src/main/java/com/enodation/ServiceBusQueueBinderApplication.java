ac90f6ec751437f5014d9ce4fff666a7158fbef7
Sandip Patil <>
2023-09-26 09:01:12 -0400

    corrected dependency version as per java8

--- service-bus/java/spring-cloud-stream/queue-send-operations/src/main/java/com/enodation/ServiceBusQueueBinderApplication.java
+++ service-bus/java/spring-cloud-stream/queue-send-operations/src/main/java/com/enodation/ServiceBusQueueBinderApplication.java
@@ -1,6 +1,5 @@
 package com.enodation;
 
-import com.azure.spring.messaging.checkpoint.Checkpointer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
